fe23d6109ed6a5471e555b1cbaa8b65e850f66de
wesleyEmidio <wesley_esc1987@hotmail.com>
2026-01-29 11:28:22 -0300

    Ajuste no HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 //exportando a função
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
-        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
