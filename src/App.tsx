143b01e13670db2d3c00a4632f490c7471ba4161
wesleyEmidio <wesley_esc1987@hotmail.com>
2026-01-29 14:19:30 -0300

    23. Criando o compotente de LOGO com icone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
-
-//exportando a função
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
-      </Container>
-
-      <Container>
-        <Heading>MENU</Heading>
+        <Logo />
       </Container>
     </>
   );
