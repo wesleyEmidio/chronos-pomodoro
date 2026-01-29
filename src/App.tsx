66a08b02172131590a582abed8d3eeae069cabb1
wesleyEmidio <wesley_esc1987@hotmail.com>
2026-01-29 14:37:10 -0300

    24. Criando componente MENU com link e icones

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
       </Container>
     </>
   );
