f85a106a069f2b7bb6207d525b351befefc078c9
wesleyEmidio <wesley_esc1987@hotmail.com>
2026-01-30 12:14:55 -0300

    30. criando componente Cycles com indicadores de ciclos promodoro com cores

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -38,8 +39,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
